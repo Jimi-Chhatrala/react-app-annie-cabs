141676d3aedf177d1fb626408e38d0dd5f776c79
Jimi-Chhatrala <jimichhatrala8488@gmail.com>
2024-06-05 17:50:35 +0530

    Search Completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./Components/NavBar";
+import Search from "./Components/Search";
 
 function App() {
   return (
     <>
       <NavBar />
+      <Search />
     </>
   );
 }
---
+++ src/Components/Search/index.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import bgImage from "../../images/bg-img-home.png";
+
+function Search() {
+  return (
+    <div
+      className="h-[500px] bg-center bg-no-repeat bg-cover flex items-end"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
+      <div className="px-4 sm:px-8 w-full">
+        <form className="w-full rounded-lg grid m-4 md:flex mx-auto max-w-4xl -mb-12 p-6 bg-white shadow-lg space-y-4 md:space-y-0 space-x-0 md:space-x-3">
+          <input
+            className="md:py-1 p-3 px-3 w-full rounded-lg bg-[#F2F2F2]"
+            type="text"
+            placeholder="Leaving From"
+            required
+          />
+          <input
+            className="md:py-1 p-3 px-3 w-full rounded-lg bg-[#F2F2F2]"
+            type="text"
+            placeholder="Going To"
+            required
+          />
+          <input
+            className="md:py-1 p-3 px-3 w-full rounded-lg bg-[#F2F2F2]"
+            type="date"
+            name="date"
+            required
+          />
+          <button className="bg-[#6EDA48] rounded-lg py-3 w-full font-semibold">
+            Search
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Search;
