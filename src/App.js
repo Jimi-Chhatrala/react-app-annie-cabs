27bccf6b8d695bee5cbd0fa8721a02a526c98f32
Jimi-Chhatrala <jimichhatrala8488@gmail.com>
2024-06-06 17:23:42 +0530

    Login Completed

--- src/App.js
+++ src/App.js
@@ -7,16 +7,18 @@ import AboutUs from "./Components/AboutUs";
 import HelpUs from "./Components/HelpUs";
 import ContactUs from "./Components/ContactUs";
 import Footer from "./Components/Footer";
+import Login from "./Components/Login";
 
 function App() {
   return (
     <>
       <NavBar />
-      <Search />
-      <Price />
-      <AboutUs />
-      <HelpUs />
-      <ContactUs />
+      {/* <Search /> */}
+      {/* <Price /> */}
+      {/* <AboutUs /> */}
+      {/* <HelpUs /> */}
+      {/* <ContactUs /> */}
+      <Login />
       <Footer />
     </>
   );
